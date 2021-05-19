bdb70b420b5e47db82f4e6b0805b8e4254d461ed
Daniel Ayala <dtayala.uniajc20152@gmail.com>
2021-05-18 22:12:17 -0500

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { ScoreFiveComponent } from './components/score-five/score-five.component';
 
 const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+  { path: 'app-score-five', component: ScoreFiveComponent },
+
   {
     path: '',
     redirectTo: 'home',
---
+++ src/app/components/score-five/score-five.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-score-five',
+  templateUrl: './score-five.component.html',
+  styleUrls: ['./score-five.component.scss'],
+})
+export class ScoreFiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
